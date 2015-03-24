d23a1d3547a6a20c666965c1966dc86f6d21d863
afghl <234696073@qq.com>
2015-03-25 00:34:07 +0800

    drag_helper

--- app/assets/javascripts/helpers/drag_helper.js
+++ app/assets/javascripts/helpers/drag_helper.js
@@ -1,5 +1,61 @@
-if (!window.helpers)
-	window.helpers = {};
+window.helpers = window.helpers || {};
+
+(function (window) {
+	var DragHelper = function() {
+		return this
+	}
+
+	DragHelper.prototype = {
+		constructor:DragHelper,
+
+		//drag which element
+		drag: function (dragEleSelector) {
+			this.$drag = $(dragEleSelector).css("cursor","move")
+			this.$drag.mousedown(_mousedownCallback(this))
+			this.$drag.mouseup(_mouseupCallback(this))
+			return this
+		},
+
+		to_move: function (targetEleSelector) {
+			this.$target = $(targetEleSelector)
+			this.$target.mousemove(_mousemoveCallback(this))
+		}
+	}
+
+	window.helpers.DragHelper = DragHelper
+
+	var _mousedownCallback = function (_this) {
+		return function(e) {
+			_this.dragFlag = true
+
+			var offset = $(this).offset()
+			var x = e.pageX - offset.left
+			var y = e.pageY - offset.top
+			// _this.originX = x
+			// _this.originY = y
+			_this.originX = e.pageX
+			_this.originY = e.pageY
+		}
+	}
+
+	var _mouseupCallback = function (_this) {
+		return function() {
+			_this.dragFlag = false
+		}
+	}
+
+	var _mousemoveCallback = function (_this) {
+		return function(e) {
+			if (!_this.dragFlag)
+				return
+			var _x = e.pageX - _this.originX
+			var _y = e.pageY - _this.originY
+			_this.$target.animate({left : _x + "px", top : _y + "px"}, 0);
+		}
+	}
+})(window)
+
+
 
 helpers.dragHelper = function() {
 	
