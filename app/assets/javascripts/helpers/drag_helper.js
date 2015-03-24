e0bef10b0513bb26675c0c52afb58521e661f792
afghl <234696073@qq.com>
2015-03-24 22:56:52 +0800

    body styling

--- app/assets/javascripts/helpers/drag_helper.js
+++ app/assets/javascripts/helpers/drag_helper.js
@@ -5,24 +5,26 @@ helpers.dragHelper = function() {
 	
 	//constructor
 	this.initialize = function (selectorText) {
-		this.$target = $(selectorText)
+		this.$target = $(selectorText).css("cursor","move")
 		this.$target.mousedown(this.mousedownCallback)
 		this.$target.mousemove(this.mousemoveCallback)
 	},
 
 	this.mousedownCallback = function (e) {
 		//this is the draggable element
-		$(this).css("cursor","move")
 		var offset = $(this).offset()
 		var x = e.pageX - offset.left
 		var y = e.pageY - offset.top
 	},
 
 	this.mousemoveCallback = function (e) {
-		console.log('mousemove...')
+		if(!this.style.left || !this.style.top){
+			this.style.left = e.pageX + 'px'
+			this.style.top = e.pageY + 'px'
+		}
 		var _x = e.pageX - parseInt(this.style.left)
 		var _y = e.pageY - parseInt(this.style.top)
-		this.style.left = _x
-		this.style.top = _y
+		this.style.left = _x + 'px'
+		this.style.top = _y + 'px'
 	}
 }
