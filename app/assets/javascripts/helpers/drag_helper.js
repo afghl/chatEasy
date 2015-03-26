f1dc2055a44c7c432c7677d61b1a93bb557c0dec
afghl <234696073@qq.com>
2015-03-26 22:35:56 +0800

    drag_helper.js

--- app/assets/javascripts/helpers/drag_helper.js
+++ app/assets/javascripts/helpers/drag_helper.js
@@ -12,7 +12,8 @@ window.helpers = window.helpers || {};
 		drag: function (dragEleSelector) {
 			this.$drag = $(dragEleSelector).css("cursor","move")
 			this.$drag.mousedown(_mousedownCallback(this))
-			this.$drag.mouseup(_mouseupCallback(this))
+			// this.$drag.mouseup(_mouseupCallback(this))
+			$(window).mouseup(_mouseupCallback(this))
 			return this
 		},
 
@@ -27,14 +28,13 @@ window.helpers = window.helpers || {};
 	var _mousedownCallback = function (_this) {
 		return function(e) {
 			_this.dragFlag = true
-
 			var offset = $(this).offset()
 			var x = e.pageX - offset.left
 			var y = e.pageY - offset.top
 			// _this.originX = x
 			// _this.originY = y
-			_this.originX = e.pageX
-			_this.originY = e.pageY
+			_this.originX = x
+			_this.originY = y
 		}
 	}
 
@@ -49,69 +49,8 @@ window.helpers = window.helpers || {};
 			if (!_this.dragFlag)
 				return
 			var _x = e.pageX - _this.originX
-			var _y = e.pageY - _this.originY
-			_this.$target.animate({left : _x + "px", top : _y + "px"}, 0);
+			var _y = e.pageY - _this.originY 
+			_this.$target.offset({top: _y, left: _x})
 		}
 	}
 })(window)
-
-
-
-helpers.dragHelper = function() {
-	
-	//constructor
-	this.initialize = function (selectorText) {
-		this.$target = $(selectorText).css("cursor","move")
-		this.$target.mousedown(this.mousedownCallback)
-		this.$target.mousemove(this.mousemoveCallback)
-	},
-
-	this.mousedownCallback = function (e) {
-		//this is the draggable element
-		var offset = $(this).offset()
-		var x = e.pageX - offset.left
-		var y = e.pageY - offset.top
-	},
-
-	this.mousemoveCallback = function (e) {
-		if(!this.style.left || !this.style.top){
-			this.style.left = e.pageX + 'px'
-			this.style.top = e.pageY + 'px'
-		}
-		var _x = e.pageX - parseInt(this.style.left)
-		var _y = e.pageY - parseInt(this.style.top)
-		this.style.left = _x + 'px'
-		this.style.top = _y + 'px'
-	}
-}
-
-$(function () {
-    new helpers.dragHelper('hi')
-    //drag .modal-header to move .modal-dialog
-    var isDragged = false
-    var diffX = 0
-    var diffY = 0
-
-    var down = function (e) {
-        if (e.target.className == 'modal-header')
-            isDragged = true
-        $(e.target).css("cursor","move")
-        var offset = $(this).offset()
-    }
-
-    var move = function (e) {
-        if (!isDragged) return
-        movingObj = $('.modal-dialog')
-        var _x = e.pageX - parseInt(movingObj[0].style.left)
-        var _y = e.pageY - parseInt(movingObj[0].style.top)
-        console.log(movingObj[0].style.top)
-        movingObj[0].style.left = _x + 'px'
-        movingObj[0].style.top = _y + 'px'
-    }
-    var up = function (e) {
-        isDragged = false
-    }
-    document.addEventListener('mousedown', down);
-    document.addEventListener('mousemove', move);
-    document.addEventListener('mouseup', up);
-})
