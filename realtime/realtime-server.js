48255bb282b3d26b99dde0c29d011e1ec365ec8f
afghl <234696073@qq.com>
2015-04-05 17:30:31 +0800

    use partial

--- realtime/realtime-server.js
+++ realtime/realtime-server.js
@@ -11,5 +11,7 @@ io.on('connection', function(socket){
 
 	socket.on('message', function(message) {
 		console.log(message)
+		//TODO send message to everyone
+		// socket.broadcast.emit('user connected');
 	})
 });
