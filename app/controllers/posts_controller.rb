64bd3eb868c3fc71a38895c2178cf2ff6d66c550
afghl <234696073@qq.com>
2015-04-05 16:06:23 +0800

    set socket_url in BE

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -63,13 +63,13 @@ class Api::PostsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def post_params
-      params.require(:post).permit(:title, :content, :likes)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def post_params
+    params.require(:post).permit(:title, :content, :likes)
+  end
 end
