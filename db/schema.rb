74532432df5eafc7f95243f1150608eb8cf50c42
afghl <234696073@qq.com>
2015-04-09 21:42:57 +0800

    post table has local field for a image

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150409121020) do
+ActiveRecord::Schema.define(version: 20150409134133) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20150409121020) do
     t.integer  "likes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "icon"
   end
 
   create_table "users", force: :cascade do |t|
